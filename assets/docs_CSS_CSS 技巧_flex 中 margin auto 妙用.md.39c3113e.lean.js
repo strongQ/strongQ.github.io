a1d6600f681ee0a510bba2d3dfd16c83f37033ff
strongQ <strongQ@users.noreply.github.com>
2023-11-29 08:06:04 +0000

    deploy: 1de40720cc6722864c38f6bfa635aea7a20ddef3

--- assets/docs_CSS_CSS 技巧_flex 中 margin auto 妙用.md.39c3113e.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as n,o as l,c as o,U as s,k as a}from"./chunks/framework.83a19234.js";const u=JSON.parse('{"title":"flex 中 margin auto 妙用","description":"","frontmatter":{},"headers":[],"relativePath":"docs/CSS/CSS 技巧/flex 中 margin auto 妙用.md","filePath":"docs/CSS/CSS 技巧/flex 中 margin auto 妙用.md","lastUpdated":1701228601000}'),p={name:"docs/CSS/CSS 技巧/flex 中 margin auto 妙用.md"},e=s("",4),t=a("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Untitled",src:"https://codepen.io/tomiaa/embed/QWByeBM?default-tab=css%2Cresult&editable=true",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/tomiaa/pen/QWByeBM">
-  Untitled</a> by tomiaa (<a href="https://codepen.io/tomiaa">@tomiaa</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),c=s("",4),r=a("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"底部吸附",src:"https://codepen.io/tomiaa/embed/dyjLJyG?default-tab=css%2Cresult&editable=true",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/tomiaa/pen/dyjLJyG">
-  底部吸附</a> by tomiaa (<a href="https://codepen.io/tomiaa">@tomiaa</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),D=[e,t,c,r];function F(y,i,C,h,d,A){return l(),o("div",null,D)}const m=n(p,[["render",F]]);export{u as __pageData,m as default};
