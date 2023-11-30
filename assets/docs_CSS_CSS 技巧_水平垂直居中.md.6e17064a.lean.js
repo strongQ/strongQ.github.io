97380d8a100b06462869be36943ae658caf41b04
strongQ <strongQ@users.noreply.github.com>
2023-11-30 01:29:23 +0000

    deploy: 3b752fc708203ecf03b81ce5600fc854da1ff04c

--- assets/docs_CSS_CSS 技巧_水平垂直居中.md.6e17064a.lean.js
+++
@@ -1,9 +0,0 @@
-import{_ as n,o as l,c as e,U as s,k as a}from"./chunks/framework.83a19234.js";const m=JSON.parse('{"title":"CSS 水平垂直居中","description":"","frontmatter":{},"headers":[],"relativePath":"docs/CSS/CSS 技巧/水平垂直居中.md","filePath":"docs/CSS/CSS 技巧/水平垂直居中.md","lastUpdated":1701307205000}'),o={name:"docs/CSS/CSS 技巧/水平垂直居中.md"},t=s("",4),p=a("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"CSS Grid 水平垂直居中",src:"https://codepen.io/tomiaa/embed/LYBGrMx?default-tab=css%2Cresult&editable=true",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/tomiaa/pen/LYBGrMx">
-  CSS Grid 水平垂直居中</a> by tomiaa (<a href="https://codepen.io/tomiaa">@tomiaa</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),c=s("",3),r=a("iframe",{height:"300",style:{width:"100%"},scrolling:"no",title:"Untitled",src:"https://codepen.io/tomiaa/embed/WNKrVzj?default-tab=css%2Cresult&editable=true",frameborder:"no",loading:"lazy",allowtransparency:"true",allowfullscreen:"true"},`
-  See the Pen <a href="https://codepen.io/tomiaa/pen/WNKrVzj">
-  Untitled</a> by tomiaa (<a href="https://codepen.io/tomiaa">@tomiaa</a>)
-  on <a href="https://codepen.io">CodePen</a>.
-`,-1),i=[t,p,c,r];function D(F,d,y,h,C,g){return l(),e("div",null,i)}const u=n(o,[["render",D]]);export{m as __pageData,u as default};
